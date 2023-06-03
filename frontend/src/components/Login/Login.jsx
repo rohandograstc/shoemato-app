8e2c1f9165e24ea72af46063519b95ad047bf30e
Ratan Deep Singh <ratan2020cs131@abesit.edu.in>
2023-06-04 02:53:35 +0530

    login updated

--- frontend/src/components/Login/Login.jsx
+++ frontend/src/components/Login/Login.jsx
@@ -1,112 +1,174 @@
-import React from 'react'
-import { Box, Button, Checkbox, FormControlLabel, FormGroup, Grid, Stack, TextField, Typography } from '@mui/material';
-import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
-import style from './Login.module.css';
-import Image from 'next/image';
-import fbIcon from '../../assets/facebook_icon.svg'
-import appleIcon from '../../assets/apple_icon.svg'
-import googleIcon from '../../assets/google_icon.svg'
+import React, { useState } from "react";
+import Link from "next/link";
+import {
+  Box,
+  Button,
+  Checkbox,
+  FormControlLabel,
+  FormGroup,
+  Grid,
+  Stack,
+  TextField,
+  Typography,
+} from "@mui/material";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { grey } from '@mui/material/colors';
+import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
+import style from "./Login.module.css";
+import Image from "next/image";
+import fbIcon from "../../assets/facebook_icon.svg";
+import appleIcon from "../../assets/apple_icon.svg";
+import googleIcon from "../../assets/google_icon.svg";
 
+function Login() {
+  const [credentials, setCredentials] = useState({
+    username: "",
+    password: "",
+    keepLogged: false,
+  });
+  const handleChange = (e) => {
+    setCredentials({ ...credentials, [e.target.name]: e.target.value });
+    console.log(credentials);
+  };
 
+  return (
+    <Box className={style.super_container}>
+      <Stack className={style.container}>
+        <Stack className={style.box}>
+          <Typography variant="h4" className={style.login_title}>
+            Login
+          </Typography>
 
-const Login=()=> {
-    return (
-        <Box className={style.super_container}>
-            <Stack className={style.container}>
-                <Stack className={style.box}>
-                    <Typography variant="h4" className={style.login_title}>
-                        Login
-                    </Typography >
-                    
-                    <TextField  label="Email or Mobile No" variant="outlined" className={style.input}  
-                    sx={inputBoxStyle} />
-                    <TextField  label="Password or OTP" variant="outlined" className={style.input}
-                    sx={inputBoxStyle}
-                    />
-                    <Typography className={style.desc_text} style={{fontWeight:"600",textDecoration:"underline"}} >
-                        Forgot your password?
-                    </Typography>
-                    <FormGroup>
-                        <FormControlLabel sx={check_box} className={style.desc_text} control={<Checkbox />} label="Keep me logged in - applies to all log in options below. More info" />
-                    </FormGroup>
-                    <Stack direction="row" spacing={3} style={{
-                        margin:"1rem 3rem 2rem 3rem",
-                        display:"flex",
-                        alignItems:"center",
-                        justifyContent:"space-evenly"
-                    }}>
-                        
-                        <Box boxShadow={3} borderRadius="50%" className={style.card} >
-                        <Image src={googleIcon} alt='Google' className={style.icons} />
-                        </Box>
-                        <Box boxShadow={3} borderRadius="50%" className={style.card} >
-                        <Image src={appleIcon} alt='Apple' className={style.icons} />
-                        </Box>
-                        <Box boxShadow={2} borderRadius="50%" className={style.card} >
-                        <Image src={fbIcon} alt='FB' className={style.icons} />
-                        </Box>
-                    </Stack>
-                    <div className={style.btn}>
-                            <button type='submit' className={style.btn}>EMAIL LOGIN<span><ArrowForwardIcon style={{fontSize:"14px"}} /></span></button>
-                        </div>
-                    <Typography className={style.desc_text} style={{marginTop:"1rem"}}>
-                        Terms And Conditions
-                    </Typography>
-                </Stack>
-            </Stack>
-        </Box>
-    )
+          <TextField
+            type="text"
+            label="Email or Mobile No"
+            variant="outlined"
+            name="username"
+            onChange={handleChange}
+            color="primary"
+            sx={inputBoxStyle}
+          />
+          <TextField
+            type="password"
+            label="Password or OTP"
+            variant="outlined"
+            name="password"
+            onChange={handleChange}
+            color="primary"
+            sx={inputBoxStyle}
+          />
+          <Typography
+            className={style.desc_text}
+            style={{ fontWeight: "600", textDecoration: "underline" }}
+          >
+            <Link href="#">Forgot your password?</Link>
+          </Typography>
+          <FormGroup>
+            <FormControlLabel
+              sx={check_box}
+              className={style.desc_text}
+              control={
+                <Checkbox
+                  onChange={() =>
+                    setCredentials({
+                      ...credentials,
+                      ["keepLogged"]: !credentials.keepLogged,
+                    })
+                  }
+                  sx={{ p: 0 }}
+                />
+              }
+              label="Keep me logged in - applies to all log in options below. More info"
+            />
+          </FormGroup>
+          <Stack
+            direction="row"
+            spacing={3}
+            style={{
+              margin: "1rem 3rem 2rem 3rem",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "space-evenly",
+            }}
+          >
+            <Box boxShadow={3} borderRadius="50%" className={style.card}>
+              <Image src={googleIcon} alt="Google" className={style.icons} />
+            </Box>
+            <Box boxShadow={3} borderRadius="50%" className={style.card}>
+              <Image src={appleIcon} alt="Apple" className={style.icons} />
+            </Box>
+            <Box boxShadow={3} borderRadius="50%" className={style.card}>
+              <Image src={fbIcon} alt="FB" className={style.icons} />
+            </Box>
+          </Stack>
+          <Stack>
+            <ThemeProvider theme={theme}>
+              <Button
+                color="primary"
+                variant="contained"
+                endIcon={<ArrowForwardIcon style={{ fontSize: "14px" }} />}
+                sx={{ display: "flex", justifyContent: "space-between" }}>
+                EMAIL LOGIN
+              </Button>
+            </ThemeProvider>
+          </Stack>
+          <Typography className={style.desc_text} style={{ marginTop: "1rem" }}>
+            <Link href='#' style={{textDecoration:'none'}}>Terms And Conditions</Link>
+          </Typography>
+        </Stack>
+      </Stack>
+    </Box>
+  );
 }
 
+const check_box = {
+  gap: 1,
+  "& .MuiFormControlLabel-root": {
+    padding: 0.2,
+  },
+  "& .MuiTypography-root": {
+    fontSize: 14,
+    margin: 0.2,
+    fontWeight: 550,
+  },
+  "& .MuiSvgIcon-root": {
+    color: "#000000",
+  },
+};
 
-const check_box={
-    "& .MuiFormControlLabel-root":{
-      padding:.2
-    },
-    "& .MuiTypography-root":{
-      fontSize:14,
-      margin:.2,
-      fontWeight:550
-    },
-    "& .MuiSvgIcon-root":{
-        color:"#000000"
-    }
-  }
+const inputBoxStyle = {
+  "& .MuiInputBase-root": {
+    height: 50,
+    my: 1,
+    borderRadius: 2,
+    fontSize: 16,
+  },
+  "& .MuiInputLabel-root": {
+    pt: 1,
+    fontSize: 12,
+  },
+};
+const btn_text = {
+  "& .MuiButton-root": {
+    justifyContent: "flex-start",
+    margin: 0.1,
+    fontWeight: 550,
+    color: "#000000",
+    backgroundColor: "#000000",
+  },
+  "& .MuiTouchRipple-root": {
+    color: "#000000",
+    backgroundColor: "#000000",
+  },
+};
 
-  const inputBoxStyle={
-    "& .MuiInputBase-root":{
-      height:22,
-      paddingTop:3,
-  paddingBottom:2,
-      marginTop:1 ,
-      marginBottom:1,
-      fontSize:16,
-      border:1.5,
-      borderRadius:1.8
-  
+const theme = createTheme({
+  palette: {
+    mode: "dark",
+    primary: {
+      main: grey[900],
     },
-    "& .MuiInputLabel-root":{
-        fontSize:12,
-        padding:.5,
-       
-    },
-    "& .MuiFormControl-root":{
-        margin:0
-    }
-  }
-const btn_text={
-    "& .MuiButton-root":{
-        justifyContent:"flex-start",
-        margin:.1,
-        fontWeight:550,
-        color:"#000000",
-        backgroundColor:"#000000"
-
-      },
-      "& .MuiTouchRipple-root":{
-        color:"#000000",
-        backgroundColor:"#000000"
-      }
-}
+  },
+});
 
 export default Login;
