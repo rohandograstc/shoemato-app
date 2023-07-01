52216382e9703831c7aa005c9883d7bcebd40811
Mihir2324 <mihirraj444@gmail.com>
2023-07-01 22:41:57 +0530

    added font style

---
+++ frontend/src/globalStyles/typoStyles.jsx
@@ -0,0 +1,23 @@
+export const headingStyle = {
+  fontFamily: "'Open Sans'",
+  fontStyle: "normal",
+  fontWeight: "600",
+  fontSize: "20px",
+  lineHeight: "160%",
+};
+
+export const subHeadingStyle = {
+  fontFamily: "'Open Sans'",
+  fontStyle: "normal",
+  fontWeight: "600",
+  fontSize: "14px",
+  lineHeight: "160%",
+};
+
+export const paraStyle = {
+  fontFamily: "'Open Sans'",
+  fontStyle: "normal",
+  fontWeight: "400",
+  fontSize: "12px",
+  lineHeight: "16px",
+};
