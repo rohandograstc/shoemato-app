a4d3d9240ce681cf31dad399ce026a6447809728
anishsingh935 <75081988+anishsingh935@users.noreply.github.com>
2023-08-02 16:18:54 +0530

    update

--- frontend/Admin/src/pages/other/index.js
+++ frontend/Admin/src/pages/other/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import { addUser } from "@/Redux/Features/user/userSlice";
 import { wrapper } from "../../Redux/store";
 import { increment } from "@/Redux/Features/counter/coutersSlice";
-import Page from "@/Roles/Example/Page";
+// import Page from "@/Roles/Example/Page";
 import { fetchUsers } from "../../Redux/Features/user/userSliceApi";
 
 export const getServerSideProps = wrapper.getServerSideProps(
@@ -15,7 +15,7 @@ export const getServerSideProps = wrapper.getServerSideProps(
 );
 
 const Other = (props) => {
-  return <Page title="Other Page" linkTo="/" />;
+  // return <Page title="Other Page" linkTo="/" />;
 };
 
 export default Other;
